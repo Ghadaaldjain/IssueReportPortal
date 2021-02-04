4888ed166885d3b7e466e8e25927723fd34fa358
ghadaaldjain <ghadaaldjain@gmail.com>
2021-02-04 11:35:11 +0300

    Initial commit

---
+++ IssueReportPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IssueReportPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
